634d67f1f3f74acd484eae9ac92745f4e9c773d1
Pabbraga <pabbragadev@gmail.com>
2025-11-10 01:04:28 -0300

    style: adjust text overflow and small style changes

--- frontend/src/app/pages/member-list/member-list.page.ts
+++ frontend/src/app/pages/member-list/member-list.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonMenuButton } from '@ionic/angular/standalone';
 import { TableListComponent } from 'src/app/components/table-list/table-list.component';
 import { Member } from 'src/app/models/member.model';
 
@@ -10,7 +10,7 @@ import { Member } from 'src/app/models/member.model';
   templateUrl: './member-list.page.html',
   styleUrls: ['./member-list.page.scss', '../../app.component.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, TableListComponent]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, TableListComponent, IonButtons, IonMenuButton]
 })
 export class MemberListPage implements OnInit {
 
